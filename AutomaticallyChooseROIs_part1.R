fed419b8730a850d8a9d1b3bc7c51c355521fb0e
ksscheuer <katescheuer@gmail.com>
2020-10-18 19:15:16 -0500

    Now choosed ROIs based on both SNR and amplitude cutoffs

--- AutomaticallyChooseROIs_part1.R
+++ AutomaticallyChooseROIs_part1.R
@@ -4,7 +4,7 @@
 
 maxkclust <- 10 #maximum number of potential clusters
 myslice <- "04-01"
-mydate <- "Test"
+mydate <- "2019-06-26"
 dateSlice <- paste(mydate,myslice,sep=" ")
 
 ############# Load libraries, load data, and make df to fill ###############
@@ -27,8 +27,11 @@ rownames(clusteraverages_all) <- paste(rep("AvgCluster"),1:maxkclust,sep="")
 
 myfilenames <- list.files(pattern=".txt")
 nostimfilenames <- myfilenames[str_detect(myfilenames,"_noStim")]
+ampfilenames <- myfilenames[str_detect(myfilenames,"_amp")]
 myfilenames <- myfilenames[!str_detect(myfilenames,"_noStim")]
+myfilenames <- myfilenames[!str_detect(myfilenames,"_amp")]
 nostimfilelist <- lapply(nostimfilenames,read.table)
+ampfilelist <- lapply(ampfilenames,read.table)
 myfilelist <- lapply(myfilenames,read.table)
 
 ################ Find SNR cutoff based on no stim file #####################
@@ -61,7 +64,7 @@ ggplot(nostimfiles,aes(x=nostimfiles$Average)) +
 
 ggsave("Step0a_SNRcutoff.jpg",width=6,height=6)
 
-######################### Average data #####################################
+######################## Average SNR data ###################################
 ############################################################################
 
 myfiles <- as.data.frame(matrix(ncol=length(myfilelist)+5,
@@ -94,7 +97,7 @@ for (i in 1:nrow(myfiles)) {
   }
 }
 
-############################# Plot data ####################################
+########################## Plot SNR data ###################################
 ############################################################################
 
 for (i in 1:(ncol(myfiles_SNRcutoff)-3)) {
@@ -114,7 +117,7 @@ for (i in 1:(ncol(myfiles_SNRcutoff)-3)) {
     scale_fill_gradientn(colors=rev(c(
       "red1","yellow1","green1","dodgerblue1","navy"))
     )
-  ggsave(paste("Step0b_IndivHeatmap_",colnames(myfiles_SNRcutoff)[i+3],".jpg",sep=""),width=6.5,height=6)
+  ggsave(paste("Step0b_IndivHeatmap_SNR_",colnames(myfiles_SNRcutoff)[i+3],".jpg",sep=""),width=6.5,height=6)
 }
 
 ggplot(myfiles,aes(x=X,y=Y)) +
@@ -133,7 +136,82 @@ ggplot(myfiles,aes(x=X,y=Y)) +
   scale_fill_gradientn(colors=rev(c(
   "red1","yellow1","green1","dodgerblue1","navy"))
 )
-ggsave("Step0c_AvgHeatmap.jpg",height=6,width=6.5)
+ggsave("Step0c_AvgHeatmap_SNR.jpg",height=6,width=6.5)
+
+#################### Average amplitude data ################################
+############################################################################
+
+ampfiles <- as.data.frame(matrix(ncol=length(ampfilelist)+5,
+                                nrow=nrow(ampfilelist[[1]])))
+colnames(ampfiles) <- c("PixelID","X","Y",
+                       gsub(".txt","",ampfilenames),
+                       "Avg","AvgGps")
+ampfiles$PixelID <- ampfilelist[[1]][,1]
+ampfiles$X <- rep(1:80,80)
+ampfiles$Y <- rep(1:80,each=80)
+
+for (i in 1:length(ampfilelist)) {
+  ampfiles[,i+3] <- ampfilelist[[i]][,2]
+}
+
+# ampfiles_SNRcutoff <- ampfiles[,c(1:(ncol(ampfiles)-2))]
+# for (i in 1:length(ampfilenames)) {
+#   for (j in 1:nrow(ampfiles_SNRcutoff)) {
+#     if (ampfiles_SNRcutoff[j,3+i] <= SNRcutoff) {
+#       ampfiles_SNRcutoff[j,3+i] <- NA
+#     }
+#   }
+# }
+
+ampfiles$Avg <- rowMeans(ampfiles[,4:(ncol(ampfiles)-2)])
+ampfiles$AvgGps <- ampfiles$Avg
+# for (i in 1:nrow(ampfiles)) {
+#   if (ampfiles$Avg[i] <= SNRcutoff) {
+#     ampfiles$AvgGps[i] <- NA
+#   }
+# }
+
+######################### Plot amplitude data ##############################
+############################################################################
+
+for (i in 1:(ncol(ampfiles)-5)) {
+# for (i in 1:(ncol(ampfiles)-3)) {
+  ggplot(ampfiles,aes(x=X,y=Y)) +
+    geom_tile(aes(fill=ampfiles[,3+i])) +
+    labs(title=paste(dateSlice)) +
+    theme(
+      axis.title.y = element_blank(),
+      axis.title.x = element_blank(),
+      axis.ticks = element_blank(),
+      axis.text = element_blank(),
+      panel.background = element_blank(),
+      legend.title = element_blank(),
+      plot.title = element_text(hjust=0.5)
+    ) +
+    scale_y_reverse() +
+    scale_fill_gradientn(colors=rev(c(
+      "red1","yellow1","green1","dodgerblue1","navy"))
+    )
+  ggsave(paste("Step0b_IndivHeatmap_Amp_",colnames(ampfiles)[i+3],".jpg",sep=""),width=6.5,height=6)
+}
+
+ggplot(ampfiles,aes(x=X,y=Y)) +
+  geom_tile(aes(fill=ampfiles$AvgGps)) +
+  labs(title=paste(dateSlice)) +
+  theme(
+    axis.title.y = element_blank(),
+    axis.title.x = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    panel.background = element_blank(),
+    legend.title = element_blank(),
+    plot.title = element_text(hjust=0.5)
+  ) +
+  scale_y_reverse() +
+  scale_fill_gradientn(colors=rev(c(
+    "red1","yellow1","green1","dodgerblue1","navy"))
+  )
+ggsave("Step0c_AvgHeatmap_Amp.jpg",height=6,width=6.5)
 
 ######### hierarchical clustering: find optimal number of clusters  ########
 ############################################################################
@@ -159,4 +237,5 @@ ggsave("Step1b_silhouetteplot.jpeg",height=6,width=6)
 ############################################################################
 
 write.csv(myclusterdata,"EachPixel_Cluster.csv",row.names = FALSE)
-write.csv(myfiles,"EachPixel_Data.csv",row.names = FALSE)
+write.csv(myfiles,"EachPixel_SNR_Data.csv",row.names = FALSE)
+write.csv(ampfiles,"EachPixel_Amp_Data.csv",row.names = FALSE)
